316ca0bcda9a3f52a1faea97f118d40ca3da2354
Emily Wilbur <ewilbur94@gmail.com>
2021-05-17 09:55:45 -0400

    table user data, api call, header

---
+++ src/components/Table.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Table(props) {
+    console.log(props);
+    const userData = props.users.map((user) => {
+        return <div>{user.name.first}</div>;
+    });
+    console.log(userData);
+    return (
+    <div className="container">
+        {userData}
+        </div>
+    );
+}
+
+
+export default Table;
+
