646500dde76fd97d5eb6ca285d3adcc786e0be7c
Emily Wilbur <ewilbur94@gmail.com>
2021-05-20 22:22:48 -0400

    format date function

--- src/components/DataBody.js
+++ src/components/DataBody.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 function DataBody({ users }) {
 
+    function formatDate(date) {
+const dateArray = date.split("-");
+const year = dateArray[0];
+const month = dateArray[1];
+const dayArray = date[1].split("-");
+const day = dayArray[0];
+const formattedDate = [month, day, year].join["-"];
+return formattedDate;
+    }
+
 
 
 }
@@ -10,7 +20,8 @@ return (
 
     <tbody>
 
-        
+
+
     </tbody>
 
 
