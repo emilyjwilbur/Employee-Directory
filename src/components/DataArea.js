e278f98dad7a3d38a0a699a4b0582cf37e5b26bd
Emily Wilbur <ewilbur94@gmail.com>
2021-05-20 21:41:43 -0400

    handleSearchChange function fix and componentDidMount fct API

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import API from '../utils/API';
 
 
 
@@ -46,11 +47,50 @@ export default class DataArea extends Component {
                 } else {
                     return a[heading] - b[heading];
                 }
+            } else {
+                if (a[heading] === undefined) {
+                    return 1;
+                } else if (b[heading] === undefined) {
+                    return -1;
+
+                }
+
+                else if (heading === "name") {
+                    return b[heading].first.localCompare(a[heading].first);
+                } else {
+                    return b[heading] - a[heading];
+                }
             }
         }
 
 
-        const 
+        const sortedUsers = this.state.filteredUsers.sort(compareFnc);
+        this.setState({filteredUsers: sortedUsers });
+
     }
 
+    handleSearchChange = event => {
+        console.log(event.target.value);
+        const filter = event.target.value;
+        const filteredList = this.state.users.filter(item => {
+            let values = Object.values(item)
+            .join("")
+            .toLowerCase();
+            return values.indexOf(filter.toLowerCase()) !== -1;
+        });
+        this.setState({filteredUsers: filteredList});
+    }
+
+
+    componentDidMount() {
+        API.getUsers().then(results => {
+            this.setState({
+                users: results.data.results,
+                filteredUsers: results.data.results
+            });
+        });
+    }
+
+
+    
 }
